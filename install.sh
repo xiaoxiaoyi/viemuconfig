48ab5eebb86fcfb865f6d571548934e65bc47829
xiaoxiaoyi <yisman1991@gmail.com>
2013-05-15 21:25:07 +0800

    auto upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash  
+#===============================================================================
+#
+#          FILE: install.sh
+# 
+#         USAGE: ./install.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2013-5-15 21:13:06 中国标准时间
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
+#复制文件夹
+cp .viemurc ~
+
